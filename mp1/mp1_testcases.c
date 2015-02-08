7b9256081f704ba4ae05f91f32ce70967546d243
dong28 <dong28@ec2bff7a-d9fd-43d6-bdc8-62d97b30f49d>
2015-02-08 07:09:37 +0000

    mp1 commit

--- mp1/mp1_testcases.c
+++ mp1/mp1_testcases.c
@@ -30,6 +30,10 @@ int main(void)
 	if(mp1_set_video_mode() == NULL) {
 		return -1;
 	}
+	for (i=0; i < 80*25*2; i+=2) { 
+	  vmem_base_addr[i] = ' '; 
+	  vmem_base_addr[i+1] = 7; 
+	}
 
 	rtc_fd = open("/dev/rtc", O_RDWR);
 
